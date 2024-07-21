10ec5f89dd34e91000d59fffab02ee35cce3307f
Liz Martins <elizabeth.martins5@gmail.com>
2024-07-21 12:31:53 -0400

    setup react router, transferred boilerplate components from freeCodeCamp lesson repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
-    <>
-      <div>Git Explorer</div>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   )
 }
 
